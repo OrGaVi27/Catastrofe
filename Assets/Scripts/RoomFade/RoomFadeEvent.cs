c84cadf4e420562fefa6a7b0e196356d9c7ef616
OrGaVi27 <calgali27@gmail.com>
2025-05-24 13:34:48 +0200

    Area Manager listo

--- Assets/Scripts/RoomFade/RoomFadeEvent.cs
+++ Assets/Scripts/RoomFade/RoomFadeEvent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationsUI : MonoBehaviour
+public class RoomFadeEvent : MonoBehaviour
 {
     Animator roomFadeAnimator;
 
