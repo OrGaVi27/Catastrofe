2cd0f480dd918af86bfd3e844893ad18b27ca470
PauSori <pausorianoj@gmail.com>
2025-06-03 19:31:21 +0200

    Decoracion y Altar
    Acabada decoracion
    Altar acabados y puestos dentro

--- Assets/Scripts/Mapa/Altar.cs
+++ Assets/Scripts/Mapa/Altar.cs
@@ -4,33 +4,48 @@ using UnityEngine;
 public class Altar : MonoBehaviour
 {
     public GameObject player;
-
+    [Space]
     public GameObject habitacion;
-
+    [Space]
     public GameObject altar;
-    public Material[] materials;
+    public GameObject altarActive;
+    public GameObject altarDesactive;
+    [Space]
+    public AudioClip healingSound;
+
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
+    void Update()
+    {
+        if (player.GetComponent<SpawnPointsPlayer>().spawn.transform.position == gameObject.transform.position)
+        {
+            altarActive.SetActive(true);
+            altarDesactive.SetActive(false);
+        }
+        else
+        {
+            altarActive.SetActive(false);
+            altarDesactive.SetActive(true);
+        }
+    }
+
     public void AltarInteract()
     {
-        altar.GetComponent<Renderer>().material = materials[1];
-
         player.GetComponent<BasePlayerStats>().currentHealth = player.GetComponent<BasePlayerStats>().maxHealth;
 
         player.GetComponent<BasePlayerStats>().currentMana = player.GetComponent<BasePlayerStats>().maxMana;
 
-        //Pociones al maxisimo
-        Debug.Log("potis max");
+        player.GetComponent<BasePlayerStats>().currentHeals = player.GetComponent<BasePlayerStats>().maxHeals;
 
         player.GetComponent<SpawnPointsPlayer>().habitacionSpawn = habitacion;
 
         player.GetComponent<SpawnPointsPlayer>().spawn.transform.position = gameObject.transform.position;
 
-        //Sonido de curar
+        ControladorSonido.Instance.EjecutarSonido(healingSound);
     }
 
     void OnTriggerEnter(Collider other)
@@ -39,10 +54,5 @@ public class Altar : MonoBehaviour
         {
             AltarInteract();
         }
-    }
-
-    void OnTriggerExit()
-    {
-        altar.GetComponent<Renderer>().material = materials[0];
     }
 }
